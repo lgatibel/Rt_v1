3883117e27036ffe6d1c9b961ccab80aaf45099d
Lionel GATIBELZA <lgatibel@e3r9p13.42.fr>
2016-11-17 16:39:27 +0100

    refactoring 3

---
+++ srcs/parse.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgatibel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/17 14:44:07 by lgatibel          #+#    #+#             */
+/*   Updated: 2016/11/17 14:47:16 by lgatibel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <rtv1.h>
+
+void				parse_file(char *file,t_env **env, t_object **object)
+{
+	int		fd;
+	char	*line;
+
+	fd = -8;
+	line = NULL;
+	*object = NULL;
+	file = "lol"; // a supprimer
+/*
+	if ((fd = open(file, O_RDONLY)) > 0)
+		error(OPEN, __LINE__, __FILE__);
+	while((get_next(fd, &line)) > 0)
+	{
+	}
+	if (close(fd) == -1)
+		error(CLOSE, __LINE__, __FILE__);
+*/
+	set_env(env);
+}
---
+++ srcs/tp3d_calc.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tp3d_calc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgatibel <lgatibel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/09 17:22:33 by lgatibel          #+#    #+#             */
+/*   Updated: 2016/11/17 14:41:06 by lgatibel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <rtv1.h>
+
+void			tp3d_cpy(t_p3d *dest,
+t_p3d src)
+{
+	dest->x = src.x;
+	dest->y = src.y;
+	dest->z = src.z;
+}
+
+t_p3d			tp3d_mult_nb(t_p3d tp3d,
+double nb)
+{
+	t_p3d		result;
+
+	result.x = tp3d.x * nb;
+	result.y = tp3d.y * nb;
+	result.z = tp3d.z * nb;
+	return (result);
+}
+
+t_p3d			tp3d_sum(t_p3d tp3d1, t_p3d tp3d2)
+{
+	t_p3d		result;
+	result.x = tp3d1.x + tp3d2.x;
+	result.y = tp3d1.y + tp3d2.y;
+	result.z = tp3d1.z + tp3d2.z;
+	return (result);
+}
+
+t_p3d			tp3d_sub(t_p3d tp3d1, t_p3d tp3d2)
+{
+	t_p3d		result;
+	result.x = tp3d1.x - tp3d2.x;
+	result.y = tp3d1.y - tp3d2.y;
+	result.z = tp3d1.z - tp3d2.z;
+	return (result);
+}
+
+double			tp3d_mult(t_p3d tp3d1, t_p3d tp3d2)
+{
+	double		result;
+	result = tp3d1.x * tp3d2.x;
+	result += tp3d1.y * tp3d2.y;
+	result += tp3d1.z * tp3d2.z;
+	return (result);
+}
