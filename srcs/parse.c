a0b03723dfe18cc7b5c3c3a15becaf5881bd1cd4
Lionel GATIBELZA <lionelgatibelza@gmail.com>
2016-11-30 18:01:29 +0100

    have to code strrstr

--- srcs/parse.c
+++ srcs/parse.c
@@ -6,7 +6,7 @@
 /*   By: lgatibel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/17 14:44:07 by lgatibel          #+#    #+#             */
-/*   Updated: 2016/11/29 19:32:31 by lgatibel         ###   ########.fr       */
+/*   Updated: 2016/11/30 18:01:03 by lgatibel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,23 @@ t_object		*manage_line(char *line, int *index)
 		error_parse(__FILE__, "no start in file", *index + 1);
 	else if (*index == 1 && ft_strcmp(line, "##CAM"))// && !set_cam(&object,*index)))
 		error_parse(__FILE__, "bad camera definition", *index + 1);
-
 	return (object);
+}
+/*
+char			*ft_strrsrr(char *big, char little)
+{
+	if (!litle)
+		return (NULL)
+}
+*/
+int				good_extension(char * file)
+{
+	char *ext;
+
+	ext = ft_strstr(file, ".rtv1");
+	if(ext && !ft_strcmp(ext, ".rtv1"))
+		return (1);
+	return (0);
 }
 
 void			parse_file(char *file, t_env **env, t_object **object)
@@ -40,15 +55,15 @@ void			parse_file(char *file, t_env **env, t_object **object)
 	int			index;
 	char		*line;
 	t_object	*obj;
-	t_object	*tmp;
 
 	fd = -8;
 	line = NULL;
 	*object = NULL;
 
-//	file = NULL;
 	obj = NULL;
 	index = 0;
+	if (!good_extension(file))
+		error_extension(".rtv1", EXIT);
 	if ((fd = open(file, O_RDONLY)) < 1)
 		error(OPEN, __LINE__, __FILE__, NO_EXIT);
 	while((get_next_line(fd, &line)) > 0)
@@ -56,7 +71,6 @@ void			parse_file(char *file, t_env **env, t_object **object)
 		manage_line(line, &index);
 		if (obj == NULL)
 			printf("null\n");
-		tmp = obj;
 		//*object = manage_line(line, object)
 //			error(INIT, __LINE__, __FILE__ );
 		index++;
