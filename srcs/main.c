991413563ee74f7f662faaa0a3c5b3e87666c452
Lionel GATIBELZA <lionelgatibelza@gmail.com>
2016-12-20 12:12:55 +0100

    little hotfix on light

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: lgatibel <lgatibel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/02 13:19:35 by lgatibel          #+#    #+#             */
-/*   Updated: 2016/12/20 12:03:14 by lgatibel         ###   ########.fr       */
+/*   Updated: 2016/12/20 12:12:38 by lgatibel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -115,11 +115,11 @@ int					calc_light(t_env *env)
 	light = &env->light;
 	//	if (env->intersect.y > 0)
 	//	printf("x[%f], y[%f], z[%f]\n", env->intersect.x, env->intersect.y, env->intersect.z);
-	normalized(&env->intersect, 1);
 	light->dir = sub_tp3d(env->intersect, light->pos);
+	normalized(&env->intersect, 1);
 	reverse_tp3d(&light->dir);
 	normalized(&light->dir, 1);
-//	normalized(&env->norm, 1);
+	normalized(&env->norm, 1);
 	angle = mult_tp3d(env->norm, light->dir);
 //	if (angle <= 0)
 //		return (0);
@@ -129,8 +129,6 @@ int					calc_light(t_env *env)
 	col = ((int)(color(env->color, RED) * diffuse) << 16) +
 	((int)(color(env->color, GREEN) * diffuse) << 8) +
 	(int)(color(env->color, BLUE) * diffuse) ;
-//	if (col < 0)
-//		col *= -1;
 	if (color > 0)
 	{
 		printf("color [%x], col[%d], angle[%f]\n", env->color, col, angle);
--- srcs/ray.c
+++ srcs/ray.c
@@ -6,7 +6,7 @@
 /*   By: lgatibel <lgatibel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 12:38:33 by lgatibel          #+#    #+#             */
-/*   Updated: 2016/12/20 12:02:54 by lgatibel         ###   ########.fr       */
+/*   Updated: 2016/12/20 12:11:40 by lgatibel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ void				calc_ray(t_env *env, double x, double y)
 
 void				set_light(t_light *light)
 {
-	set_tp3d(&light->pos, -50, 0, 480);
+	set_tp3d(&light->pos, 0, 0, 400);
 	set_tp3d(&light->dir, 0, 0, 0);
 }
 
